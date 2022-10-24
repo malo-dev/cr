b80b9a9cb8a6d1050abf3f0aa4e2ccd2bee8b1dd
malo-dev <leadermushio377@gmail.com>
2022-10-24 13:10:10 +0200

    changes

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,3 +1,7 @@
 exports.get_landing = function(req, res, next) {
   res.render('landing', { title: 'malo' });
 }
+exports.submit_lead = function (req, res, next) {
+  res.redirect('/')
+ console.log("lead email : ",req.body.lead_email )
+}
